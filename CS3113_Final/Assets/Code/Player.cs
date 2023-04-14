049bbb746d534889c895c7ae7c137e81a5d60411
Sabrina Li <swl416@nyu.edu>
2023-04-14 17:53:12 -0400

    unfroze player x pos + added carrotpattern1 back to spawn list

--- CS3113_Final/Assets/Code/Player.cs
+++ CS3113_Final/Assets/Code/Player.cs
@@ -37,7 +37,8 @@ public class Player : MonoBehaviour
     {
         if (Input.GetMouseButton(0)){
             _rigidbody.AddForce(new Vector3(0, 50, 0), ForceMode2D.Force);
-        } else if (Input.GetMouseButtonUp(0)){
+        } 
+        else if (Input.GetMouseButtonUp(0)){
             _rigidbody.velocity *= 0.50f;
         }
         if (transform.position.x - camera.position.x <= -2.8f)
