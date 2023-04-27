80be6ae54ce7bf7df31eef0faf994eac95300459
Ashley <afl331@nyu.edu>
2023-04-27 14:47:04 -0400

    Add shop to menu and update carrotsUI glitch

--- CS3113_Final/Assets/Code/ScoreManager.cs
+++ CS3113_Final/Assets/Code/ScoreManager.cs
@@ -16,28 +16,27 @@
      {
          score = 0;
          highscore = PlayerPrefs.GetInt ("highscore", highscore);
-         scoreUI.text = "score: " + score + "\nhighscore: " + highscore;
+         scoreUI.text = "score: " + score + "\nhighscore: " + highscore + "\ncarrots: " + carrots;
          int startingPosX = (int)player.transform.position.x;   
      }
 
      void FixedUpdate(){
+        carrots = PlayerPrefs.GetInt("carrots", carrots);
         if (GetComponent<GameManager>().GetLives() > 0) {
             score = (int)player.transform.position.x - startingPosX;
             scoreUI.text = "score: " + score + "\nhighscore: " + highscore + "\ncarrots: " + carrots;
             if (score > highscore){
                 highscore = score;
-                scoreUI.text = "score: " + score + "\nhighscore: " + highscore;
+                scoreUI.text = "score: " + score + "\nhighscore: " + highscore + "\ncarrots: " + carrots;
                 PlayerPrefs.SetInt ("highscore", highscore);
             }
-
-            PlayerPrefs.SetInt ("carrots", carrots);
         }
      }
  
      public static void AddPoints (int pointsToAdd)
      {
-         score += pointsToAdd;
-         carrots += pointsToAdd;
+        //  score += pointsToAdd;
+        //  carrots += pointsToAdd;
      }
  
      public static void Reset()
